417a7a834bc48b3ad4b622e4c2e0c62b71ab6426
Erica Monteiro Diogo <erica.monteiro.diogo@gmail.com>
2024-09-24 22:41:39 -0400

    Added edit post dialog.

--- app/src/main/java/com/example/withme_android/User_PostView.java
+++ app/src/main/java/com/example/withme_android/User_PostView.java
@@ -15,10 +15,6 @@ public class User_PostView extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_user_post_view);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
     }
 }
