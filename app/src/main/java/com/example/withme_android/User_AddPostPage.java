7bd4aa7a37af9e8a195b572ce0137b8e1a9cd60e
Muthu <muthusamycanada@gmail.com>
2024-10-02 15:08:53 +0000

    post update without java problem in java code for post

--- app/src/main/java/com/example/withme_android/User_AddPostPage.java
+++ app/src/main/java/com/example/withme_android/User_AddPostPage.java
@@ -1,24 +1,137 @@
 package com.example.withme_android;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.bumptech.glide.Glide;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class User_AddPostPage extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private DatabaseReference reference;
+    private ImageView homeIcon, searchIcon, addPostIcon, smallAvatar, bigAvatar;
+    private TextView userName;
+    private Button btnPost;
+
+    private User userData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_user_add_post_page);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        mAuth = FirebaseAuth.getInstance();
+        reference = FirebaseDatabase.getInstance().getReference("users");
+        homeIcon = findViewById(R.id.homeIcon);
+        searchIcon = findViewById(R.id.searchIcon);
+        addPostIcon = findViewById(R.id.addPostIcon);
+        smallAvatar = findViewById(R.id.smallAvatar);
+        bigAvatar = findViewById(R.id.iv_profile);
+        userName = findViewById(R.id.tv_user_name);
+        btnPost = findViewById(R.id.btn_post);
+
+        retrieveInfo();
+
+        homeIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(User_AddPostPage.this, User_HomePage.class);
+                startActivity(intent);
+                finish();
+            }
         });
+
+        searchIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(User_AddPostPage.this, User_SearchPage.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        addPostIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(User_AddPostPage.this, User_AddPostPage.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        smallAvatar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(User_AddPostPage.this, User_ProfilePage.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        btnPost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createPost();
+            }
+        });
+    }
+
+    private void createPost() {
+
+    }
+
+    private void retrieveInfo() {
+        FirebaseUser user = mAuth.getCurrentUser();
+
+        if (user != null) {
+            reference.child(mAuth.getUid()).addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    User userProfile = snapshot.getValue(User.class);
+                    if (userProfile != null) {
+                        userData = userProfile;
+
+                        String name = userProfile.getName();
+                        String userAvatar = userProfile.getUserPhotoUrl();
+
+                        userName.setText(name);
+
+                        Glide.with(bigAvatar.getContext())
+                                .load(userAvatar)
+                                .error(R.drawable.round_report_problem_24)
+                                .fitCenter()
+                                .into(bigAvatar);
+
+                        Glide.with(smallAvatar.getContext())
+                                .load(userAvatar)
+                                .error(R.drawable.round_report_problem_24)
+                                .fitCenter()
+                                .into(smallAvatar);
+                    }
+                }
+                // hi
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+                    Toast.makeText(User_AddPostPage.this, "Failed to load user data.", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
     }
 }
--- app/src/main/java/com/example/withme_android/User_HomePage.java
+++ app/src/main/java/com/example/withme_android/User_HomePage.java
@@ -1,61 +1,110 @@
 package com.example.withme_android;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-
 public class User_HomePage extends AppCompatActivity {
 
-    private RecyclerView rvPost;
-    private List<Post> posts;
-    private PostAdapter postAdapter;
-
-    private FirebaseUser firebaseUser;
-    private FirebaseDatabase firebaseDatabase;
-
-    private User user;
-
+    private FirebaseAuth mAuth;
+    private DatabaseReference reference;
+    private ImageView homeIcon, searchIcon, addPostIcon, smallAvatar;
+    
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_user_home_page);
 
-        rvPost = findViewById(R.id.rv_post_home);
+        mAuth = FirebaseAuth.getInstance();
+        reference = FirebaseDatabase.getInstance().getReference("users");
+        homeIcon = findViewById(R.id.homeIcon);
+        searchIcon = findViewById(R.id.searchIcon);
+        addPostIcon = findViewById(R.id.addPostIcon);
+        smallAvatar = findViewById(R.id.smallAvatar);
 
-        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        retrieveInfo();
+
+        homeIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(User_HomePage.this, User_HomePage.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        searchIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(User_HomePage.this, User_SearchPage.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        addPostIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(User_HomePage.this, User_AddPostPage.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        smallAvatar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(User_HomePage.this, User_ProfilePage.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+    }
 
 
+    private void retrieveInfo() {
+        FirebaseUser user = mAuth.getCurrentUser();
 
+        if (user != null) {
+            reference.child(mAuth.getUid()).addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    User userProfile = snapshot.getValue(User.class);
+                    if (userProfile != null) {
 
+                        String userAvatar = userProfile.getUserPhotoUrl();
 
-        // create few dummy posts
-        posts = new ArrayList<>();
-
-        Post post1 = new Post("Hello there, this is my first post", firebaseUser.getUid(), "",
-                new Date().toString(), );
-        posts.add(post1);
-
-
-        // Set the adapter and layout manager
-        postAdapter = new PostAdapter(posts, this);
-        rvPost.setAdapter(postAdapter);
-        rvPost.setLayoutManager(new LinearLayoutManager(this));
+                        Glide.with(smallAvatar.getContext())
+                                .load(userAvatar)
+                                .error(R.drawable.round_report_problem_24)
+                                .fitCenter()
+                                .into(smallAvatar);
+                    }
+                }
+                // hi
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+                    Toast.makeText(User_HomePage.this, "Failed to load user data.", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
     }
 }
