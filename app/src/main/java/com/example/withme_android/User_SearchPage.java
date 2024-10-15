44414d2464e08bb13ee4611322540f1ba4f12a4d
Fadrick Barroso <89845529+Fadrickmmb@users.noreply.github.com>
2024-10-14 23:20:08 -0400

    Basic Functions for User Search Page

--- app/src/main/java/com/example/withme_android/User_SearchPage.java
+++ app/src/main/java/com/example/withme_android/User_SearchPage.java
@@ -1,6 +1,12 @@
 package com.example.withme_android;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +15,12 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class User_SearchPage extends AppCompatActivity {
+
+    EditText searchInput;
+    ImageView searchButton;
+    TextView result01, result02, result03;
+    Button toHome;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +32,23 @@ public class User_SearchPage extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        searchInput = findViewById(R.id.user_searchPage_input);
+        searchButton = findViewById(R.id.user_searchPage_searchButton);
+        result01 = findViewById(R.id.user_searchPage_result01);
+        result02 = findViewById(R.id.user_searchPage_result02);
+        result03 = findViewById(R.id.user_searchPage_result03);
+        toHome = findViewById(R.id.user_searchPage_buttonToHome);
+
+        toHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), User_HomePage.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
     }
 }
