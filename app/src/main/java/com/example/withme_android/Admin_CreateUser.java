49f6ed29b413006b9bded3781b5bd46a34c4a44a
Fadrick Barroso <89845529+Fadrickmmb@users.noreply.github.com>
2024-10-20 21:43:59 -0400

    Working on Creation of Admin and Mod

--- app/src/main/java/com/example/withme_android/Admin_CreateUser.java
+++ app/src/main/java/com/example/withme_android/Admin_CreateUser.java
@@ -1,9 +1,6 @@
 package com.example.withme_android;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,30 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Admin_HomePage extends AppCompatActivity {
-
-    Button toCreate;
+public class Admin_CreateUser extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_admin_home_page);
+        setContentView(R.layout.activity_admin_create_user);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
-
-        toCreate = findViewById(R.id.admin_homePage_toCreate);
-
-        toCreate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), Admin_CreateUser.class);
-                startActivity(intent);
-                finish();
-            }
         });
     }
 }
